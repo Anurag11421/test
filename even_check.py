797f30d621e9bd16f264e3b0c78ca0a3f9c53fa1
Anurag11421 <anuragrangu11421@gmail.com>
2021-06-24 10:44:38 +0530

    even-no

---
+++ even_check.py
@@ -0,0 +1,10 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
+def even_check(num):
+    even_no = []
+
+    for number in num:
+        if number % 2 == 0:
+            even_no.append(number)
+        else:
+            pass 
